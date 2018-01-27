00a5ed1ab9021101b82f67973c53fb7e94262cb7
Tavish Aggarwal <tavishaggarwal1993@gmail.com>
2018-01-28 00:31:10 +0530

    Corrected case senstivity

--- Server/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '',
-    'user': 'ussysmg.worldinsight@gmail.com',
-    'pass': '',
-    'Domain': 'https://worldinsight.mybluemix.net',
-    'mailSend': true,
-    'tokenExpire': 15 // Time in minutes
-};
